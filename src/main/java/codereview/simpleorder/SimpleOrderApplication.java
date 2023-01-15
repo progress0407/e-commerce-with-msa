cadfa1cd96936af0b230a6860929e636720ade44
progress0407 <progress0407@gmail.com>
2023-01-15 10:23:43 +0900

    feat: create spring project for java 17

---
+++ src/main/java/codereview/simpleorder/SimpleOrderApplication.java
@@ -0,0 +1,13 @@
+package codereview.simpleorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleOrderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/codereview/simpleorder/SimpleOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package codereview.simpleorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
