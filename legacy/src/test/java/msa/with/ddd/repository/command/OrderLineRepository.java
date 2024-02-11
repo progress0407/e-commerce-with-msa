43c833415125973524451d3e176f092f05194736
progress0407 <progress0407@gmail.com>
2024-02-11 15:11:36 +0900

    Legacy 모듈 제거

--- legacy/src/test/java/msa/with/ddd/repository/command/OrderLineRepository.java
+++
@@ -1,8 +0,0 @@
-package msa.with.ddd.repository.command;
-
-import msa.with.ddd.order.domain.OrderItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderLineRepository extends JpaRepository<OrderItem, Long> {
-}
-
