aaf950a3fde7c9090641131057fd44174001789b
progress0407 <progress0407@gmail.com>
2024-02-11 21:20:56 +0900

    [Item][bug-fix] 상품 이벤트 리스너 버그 수정

--- item/src/main/kotlin/io/philo/shop/queue/consumer/OrderCreatedEventListener.kt
+++ item/src/main/kotlin/io/philo/shop/queue/consumer/OrderCreatedEventListener.kt
@@ -1,7 +1,7 @@
 package io.philo.shop.queue.consumer
 
 import io.philo.shop.domain.service.ItemService
-import io.philo.shop.order.OrderLineCreatedEvent
+import io.philo.shop.order.OrderCreatedEvent
 import io.philo.shop.order.OrderRabbitProperty.Companion.QUEUE_NAME
 import mu.KotlinLogging
 import org.springframework.amqp.rabbit.annotation.RabbitListener
@@ -13,20 +13,27 @@ class OrderCreatedEventListener(private val itemService: ItemService) {
     private val log = KotlinLogging.logger { }
 
     @RabbitListener(queues = [QUEUE_NAME])
-    fun listenEvent(event: OrderLineCreatedEvent) {
+    fun listenEvent(event: OrderCreatedEvent) {
 
         log.info { "$event" }
 
-        val itemVerification = itemService.checkItemBeforeOrder(
-            event.itemId,
-            event.itemAmount,
-            event.itemQuantity
-        )
+        var finalVerify = true
+        for (event in event.orderLineCreatedEvents) {
+            val itemVerification = itemService.checkItemBeforeOrder(
+                event.itemId,
+                event.itemAmount,
+                event.itemQuantity
+            )
+            if (itemVerification == false) {
+                finalVerify=false
+            }
+        }
 
-        if(itemVerification)
-            println("todo")
+        println("finalVerify = ${finalVerify}")
+//        if(finalVerify)
+//            println("todo")
 //            itemService.decreaseItems()
 
-        println("orderable = ${itemVerification}")
+//        println("orderable = ${itemVerification}")
     }
 }
